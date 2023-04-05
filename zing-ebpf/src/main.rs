a870b315daa868844b15096e0a0cfeaa9110ffc4
Ryan Prairie <me@ryanprairie.com>
2023-04-05 19:27:56 -0400

    Add: ICMP packet filtering
    this filters icmp packet filtering. all that should be left is
    constructing the reply packet.

--- zing-ebpf/src/main.rs
+++ zing-ebpf/src/main.rs
@@ -4,20 +4,60 @@
 use aya_bpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
 use aya_log_ebpf::info;
 
+use core::mem;
+use network_types::{
+    eth::{EthHdr, EtherType},
+    icmp::IcmpHdr,
+    ip::{IpProto, Ipv4Hdr},
+};
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
+
 #[xdp(name = "zing")]
-pub fn zing(ctx: XdpContext) -> u32 {
-    match try_zing(ctx) {
+pub fn xdp_firewall(ctx: XdpContext) -> u32 {
+    match try_xdp_firewall(ctx) {
         Ok(ret) => ret,
         Err(_) => xdp_action::XDP_ABORTED,
     }
 }
 
-fn try_zing(ctx: XdpContext) -> Result<u32, u32> {
-    info!(&ctx, "received a packet");
-    Ok(xdp_action::XDP_PASS)
+#[inline(always)] //
+fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+
+    Ok((start + offset) as *const T)
 }
 
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    unsafe { core::hint::unreachable_unchecked() }
+fn try_xdp_firewall(ctx: XdpContext) -> Result<u32, ()> {
+    let ethhdr: *const EthHdr = ptr_at(&ctx, 0)?;
+    match unsafe { (*ethhdr).ether_type } {
+        EtherType::Ipv4 => {}
+        _ => return Ok(xdp_action::XDP_PASS),
+    }
+
+    let ipv4hdr: *const Ipv4Hdr = ptr_at(&ctx, EthHdr::LEN)?;
+    let source_addr = u32::from_be(unsafe { (*ipv4hdr).src_addr });
+    match unsafe { (*ipv4hdr).proto } {
+        IpProto::Icmp => {}
+        _ => return Ok(xdp_action::XDP_PASS),
+    }
+
+    let icmphdr: *const IcmpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+    let icmp_type = u8::from_be(unsafe { (*icmphdr).type_ });
+
+    info!(
+        &ctx,
+        "SRC IP: {:ipv4}, ICMP type: {}", source_addr, icmp_type
+    );
+
+    Ok(xdp_action::XDP_PASS)
 }
